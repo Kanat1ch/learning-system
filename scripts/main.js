0e7f7ea9cda9ffce57e395e64e84312e9464814c
Kanat1ch1 <ilyakanatnikov@gmail.com>
2021-05-14 18:12:38 +0300

    2.0.3: mobile auth, add image upload button

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,31 @@
-const profile = document.querySelector('.header__profile');
-const profileMenu = document.querySelector('.profile-menu');
-
-profile.addEventListener('click', e => {
-  if (!e.target.closest('.profile-menu__link'))
-  profile.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.querySelector('.header__profile')) {
+    const profile = document.querySelector('.header__profile');
+    
+    profile.addEventListener('click', e => {
+      if (!e.target.closest('.profile-menu__link'))
+      profile.classList.toggle('active');
+    })
+    
+    document.addEventListener('click', e => {
+      if (!e.target.closest('.header__profile')) {
+        profile.classList.remove('active');
+      }
+    });
+    
+  } else {
+    const profile = document.querySelector('.header__auth');
+    
+    profile.addEventListener('click', e => {
+      if (!e.target.closest('.header__links a'))
+      profile.classList.toggle('active');
+    })
+    
+    document.addEventListener('click', e => {
+      if (!e.target.closest('.header__auth')) {
+        profile.classList.remove('active');
+      }
+    });
+  }
 })
 
-document.addEventListener('click', e => {
-  if (!e.target.closest('.header__profile')) {
-    profile.classList.remove('active');
-  }
-});
